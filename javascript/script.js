727fa177d5549d9bd9257073e8afc01f5534d136
sharayu866911 <sharayuingle28@gmail.com>
2024-04-27 16:22:23 +0530

    NewPortfolio
    NewPortfolio

---
+++ javascript/script.js
@@ -0,0 +1,57 @@
+// console.log("I am student");
+
+// var name=prompt("I am taking input");
+// console.log(name);
+// for(let i=1;i<=10;i++){
+    // console.log(i);
+   
+    // console.log(i*2);
+
+
+// let n=1;
+// while(n<=10){
+//     console.log(n);
+//     n++;
+// }
+
+// let n=prompt("give me number");
+// if(n%2==0){
+//     console.log("I am even");
+// }
+// else{
+//     console.log("I am odd");
+// }
+
+// let marks=prompt("enter the number");
+// if(marks>80 && marks<=90){
+// console.log("A Grade");
+// }
+// else if(marks>70 && marks<=80){
+//     console.log("c Grade");
+// }
+// else if(marks>60 && marks<=70){
+//     console.log("C grade");
+// }
+// else{
+//     console.log(" D Grade");
+// }
+
+// 
+
+// guess the nmae
+
+// const favMovie="avenger";
+// let guess=prompt("Guess my favourite");
+// while((guess!=favMovie) && (guess!="quit")){
+//     guess=prompt("Wrong Answer!! Please try again!!");
+// }
+// if(guess==favMovie){
+//     console.log("congrats");
+// }
+
+// else{
+//     console.log("You Quited");
+// }
+
+document.querySelector(".demo").computedStyleMap.
+
